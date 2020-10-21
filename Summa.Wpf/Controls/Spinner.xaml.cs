52ffc179de25aa9c20a20fb9a58d7e62978fdedf
fabio <fabio@quantiricaferrara.onmicrosoft.com>
2020-10-22 00:27:29 +0200

    Implemented Spinner. Implemented DoubleSpinner.

--- Summa.Wpf/Controls/Spinner.xaml.cs
+++ Summa.Wpf/Controls/Spinner.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows.Controls;
 
 namespace Summa.Wpf
 {
@@ -9,9 +10,12 @@ namespace Summa.Wpf
     public partial class Spinner : UserControl
     {
 
-        #region Dependency properties
+        #region Events
 
-
+        /// <summary>
+        /// Fired when the spinner is spinned.
+        /// </summary>
+        public event EventHandler<SpinEventArgs> Spin;
 
         #endregion
 
@@ -33,8 +37,85 @@ namespace Summa.Wpf
 
         #endregion
 
+        #region Public methods
+
+        /// <summary>
+        /// Called when the content template has to be applied.
+        /// </summary>
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            UpButton.Click += (s, e) =>
+            {
+                RaiseSpin(SpinDirection.Up);
+            };
+
+            DownButton.Click += (s, e) =>
+            {
+                RaiseSpin(SpinDirection.Down);
+            };
+
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private void RaiseSpin(SpinDirection direction)
+        {
+
+            Spin?.Invoke(this, new SpinEventArgs(direction));
+
+        }
+
+        #endregion
+
     }
 
+    /// <summary>
+    /// Defines the <see cref="Spinner"/> spin event args.
+    /// </summary>
+    public class SpinEventArgs : EventArgs
+    {
 
+        #region Properties
+
+        /// <summary>
+        /// Event spin direction.
+        /// </summary>
+        public SpinDirection Direction { get; }
+
+        #endregion
+
+        #region Ctors
+
+        /// <summary>
+        /// Instantiates a new <see cref="SpinEventArgs"/> with the given direction.
+        /// </summary>
+        /// <param name="direction"> Spin direction. </param>
+        public SpinEventArgs(SpinDirection direction)
+        {
+            Direction = direction;
+        }
+
+        #endregion
+
+    }
+
+    /// <summary>
+    /// Defines the <see cref="Spinner"/> spin direction.
+    /// </summary>
+    public enum SpinDirection
+    {
+        /// <summary>
+        /// Up.
+        /// </summary>
+        Up,
+        /// <summary>
+        /// Down.
+        /// </summary>
+        Down
+    }
 
 }
