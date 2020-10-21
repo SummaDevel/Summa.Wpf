31961bab54b2e61cddaeb194b59afa4a3bdf3272
fabio <fabio@quantiricaferrara.onmicrosoft.com>
2020-10-21 23:45:45 +0200

    Aggiungere i file di progetto.

---
+++ Summa.Wpf/Controls/Spinner.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows.Controls;
+
+namespace Summa.Wpf
+{
+
+    /// <summary>
+    /// Defines a spinner.
+    /// </summary>
+    public partial class Spinner : UserControl
+    {
+
+        #region Dependency properties
+
+
+
+        #endregion
+
+        #region Properties
+
+
+
+        #endregion
+
+        #region Ctors
+
+        /// <summary>
+        /// Instantiates a new <see cref="Spinner"/>.
+        /// </summary>
+        public Spinner()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+    }
+
+
+
+}
