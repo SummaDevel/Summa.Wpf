8fd9f841b84e16a8bf13a9080747eebc7ef0e376
fabio <fabio@quantiricaferrara.onmicrosoft.com>
2020-10-31 15:18:25 +0100

    Implementato SolidColorBrushSource.

---
+++ Summa.Wpf/Sources/SolidColorBrushSource.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using System.Windows.Media;
+
+namespace Summa.Wpf
+{
+    /// <summary>
+    /// Defines a <see cref="SolidColorBrush"/> source.
+    /// </summary>
+    public class SolidColorBrushSource : ItemsSource
+    {
+
+        #region Ctors
+
+        /// <summary>
+        /// Instantiates a new <see cref="SolidColorBrushSource"/>.
+        /// </summary>
+        public SolidColorBrushSource()
+        {
+
+            foreach (var property in typeof(Colors).GetProperties(BindingFlags.Static | BindingFlags.Public))
+            {
+                Add(property.Name, new SolidColorBrush((Color)property.GetValue(null)));
+            }
+
+        }
+
+        #endregion
+
+    }
+
+}
